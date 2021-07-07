870a1b6d5765672217f167c70702adb8d63019bf
Amdadur <amdad@live.co.uk>
2021-07-07 14:02:47 +0100

    tutorial complete

---
+++ script.js
@@ -0,0 +1,23 @@
+//create new div + assign id 
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll("img"); //all images
+images.forEach((image) => {
+    // for each image, run this
+    image.addEventListener("click", (e) => {
+        lightbox.classList.add("active");
+        const img = document.createElement("img");
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+    })
+})
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove("active");
+});
